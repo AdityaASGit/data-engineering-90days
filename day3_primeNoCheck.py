d7b3e532347f40beac8779b0af26bda8c422ee80
AdityaASGit <Aditya.A.Shastri@gmail.com>
2025-06-16 23:03:41 +0530

    prime no error solved

--- day3_primeNoCheck.py
+++ day3_primeNoCheck.py
@@ -5,7 +5,7 @@ if number <= 1:
      is_primeNo = False
 else: 
     for i in range(2, int(number ** 0.5)+1):      
-        if num % i == 0:
+        if number % i == 0:
             is_primeNo = False     
             break
 if is_primeNo:
